ea96b2972ecd239307b39a1145acdcda33a976fe
samG406 <115160100+samG406@users.noreply.github.com>
2026-03-02 14:25:00 -0500

    chore(front-end): enable next/font in Next config

--- Front-end/next.config.ts
+++ Front-end/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Use this app's lockfile; avoid picking up C:\Users\gajul\package-lock.json
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
